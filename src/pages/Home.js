956fb6febce05922d8aacbaf1b06d5b9b609d4fa
Jonathan Dionne <miami.dionne@gmail.com>
2021-08-12 17:06:11 -0400

    removed components that werent being used

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-import Paper from '@material-ui/core/Paper';
 
 import axios from 'axios'
 
